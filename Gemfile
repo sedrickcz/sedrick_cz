58e5729d61c7fc47dc278facf6d55d2d54712653
Jiří Kratochvíl <sedrickcz@gmail.com>
2013-09-13 11:55:08 +0200

    + Add postmark mailer

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+ruby '2.0.0'
 
 gem 'rails', '3.2.14'
 
@@ -17,7 +18,7 @@ group :development do
 end
 
 gem 'pg'
-
+gem 'postmark-rails'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,22 +56,10 @@ SedrickCz::Application.configure do
   #Added per active admin install instructions
   config.action_mailer.default_url_options = { :host => 'http://sedrick-cz.herokuapp.com' }
 
-
-#These settings are for the sending out email for active admin and consequently the   devise mailer
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.perform_deliveries = true
-  ActionMailer::Base.raise_delivery_errors = true
-  ActionMailer::Base.smtp_settings = 
-  {
-
-    :address            => 'smtp.gmail.com',
-    :port               => 587,
-    :domain             => 'gmail.com', #you can also use google.com
-    :authentication     => :plain,
-    :user_name          => 'kratoj@gmail.com',
-    :password           => 'Simonka1989',
-    :enable_starttls_auto => true
-}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method   = :postmark
+  config.action_mailer.postmark_settings = { :api_key => ENV['POSTMARK_API_KEY'] }
 
   # Enable threaded mode
   # config.threadsafe!
